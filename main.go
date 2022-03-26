3a630bc7b2c93f5447160f53ee04cdc65d325cf5
Ali Furkan GÖKÇE <alifurkangokce@gmail.com>
2022-03-26 12:15:57 +0300

    Finish Auth and Tokens

--- main.go
+++ main.go
@@ -9,18 +9,22 @@ import (
 
 func main() {
 	port := os.Getenv("PORT")
+
 	if port == "" {
 		port = "8000"
 	}
+
 	router := gin.New()
 	router.Use(gin.Logger())
 	routers.UserRouters(router)
 	router.Use(middleware.Authentication())
+
 	routers.FoodRouters(router)
 	routers.MenuRouters(router)
 	routers.TableRouters(router)
 	routers.OrderRouters(router)
 	routers.OrderItemRouters(router)
 	routers.InvoiceRouters(router)
-	router.Run(":", port)
+
+	router.Run(":" + port)
 }
